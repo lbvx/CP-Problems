a9f16c4ab8c701327adfee86bfbbe84493f1c4a9
lbvx <lucasbraunxav@gmail.com>
2022-11-23 17:48:10 -0300

    lista 2 J TLE

---
+++ lista2/J.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+
+int main()
+{
+    int n;
+    std::pair<int, int> v;
+    std::stack<std::pair<int, int>> st;
+    std::string in;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> in;
+        if (in[1] == 'U') // PUSH
+        {
+            std::cin >> v.first;
+            if (st.empty())
+                v.second = v.first;
+            else
+                v.second = (st.top().second < v.first) ? st.top().second : v.first;
+            st.push(v);
+        }
+        else if (in[1] == 'O') // POP
+        {
+            if (!st.empty())
+                st.pop();
+            else
+                std::cout << "EMPTY" << "\n";
+        }
+        else if (in[1] == 'I') // MIN
+        {
+            if (!st.empty())
+                std::cout << st.top().second << "\n";
+            else
+                std::cout << "EMPTY" << "\n";
+        }
+    }
+    return 0;
+}
