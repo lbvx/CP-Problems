006fba427de4963a1260ecc04d82c86e6e47ff61
lbvx <lucasbraunxav@gmail.com>
2023-05-16 22:10:51 -0300

    lista4 H

---
+++ 2023.1-Matematica/lista4/H.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int msb(ll x)
+{
+    int b = 0;
+    while (x >>= 1)
+        b++;
+    return b;
+}
+
+int main()
+{
+    int n;
+    while(cin >> n)
+        cout << msb(n) + 1 << endl;
+    return 0;
+}
