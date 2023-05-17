7e4d304bad612a051718c01ebb195d6ac1094a1f
lbvx <lucasbraunxav@gmail.com>
2023-05-16 23:25:09 -0300

    lista4 I

---
+++ 2023.1-Matematica/lista4/I.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int msb(ll x)
+{
+    int b = 0;
+    while (x >>= 1)
+        b++;
+    return b;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    while(n > 0)
+    {
+        cout << msb(n) << endl;
+        cin >> n;
+    }
+    return 0;
+}
